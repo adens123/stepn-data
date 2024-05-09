0d529fe62915bb914c0977e6b2c0e8bf0ed80522
Jacky <adens123@gmail.com>
2024-05-09 16:42:34 +0800

    Fixed some issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const priceData = require("../getCM_price.js");
-let count = 0;
 
 exports.home = async (req, res) => {
   try {
@@ -8,9 +7,6 @@ exports.home = async (req, res) => {
     const mintCost = 50 + (380 * gst.price) / gmt.price + scroll.price * 2;
 
     res.render("home", { gmt, gst, sneaker, scroll, mintCost });
-
-    count++;
-    console.log(`第 ${count} 次更新資料`);
   } catch (err) {
     console.log(err);
     res.send("error");
