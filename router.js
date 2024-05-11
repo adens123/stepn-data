5537b68681a7348f8c92336fa9b00d6a962dc7c1
Jacky <adens123@gmail.com>
2024-05-11 17:21:19 +0800

    Deleted some route

--- router.js
+++ router.js
@@ -4,10 +4,4 @@ const userController = require("./controllers/userController");
 
 router.get("/", userController.home);
 
-router.get("/browser", async (req, res) => {
-  const priceData = require("./getCM_price.js");
-  let data = await priceData();
-  res.json(data);
-});
-
 module.exports = router;
